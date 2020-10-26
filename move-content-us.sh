0f38b2f03dfb13cdebae9e6d24eeb532c004c0b7
midzer <midzer@gmail.com>
2020-10-26 23:39:55 +0100

    add move-content-us.sh

---
+++ move-content-us.sh
@@ -0,0 +1,15 @@
+# Move content folders
+# Usage: move-content-dach.sh SRCFOLDER DSTFOLDER
+if [ $# -lt 2 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  exit 2
+elif [ $# -gt 2 ]; then
+  echo 1>&2 "$0: too many arguments"
+  exit 2
+fi
+
+for region in "alabama" "alaska" "arizona" "arkansas" "california" "colorado" "connecticut" "delaware" "district-of-columbia" "florida" "georgia" "hawaii" "idaho" "illinois" "indiana" "iowa" "kansas" "kentucky" "louisiana" "maine" "maryland" "massachusetts" "michigan" "minnesota" "mississippi" "missouri" "montana" "nebraska" "nevada" "new-hampshire" "new-jersey" "new-mexico" "new-york" "north-carolina" "north-dakota" "ohio" "oklahoma" "oregon" "pennsylvania" "puerto-rico" "rhode-island" "south-carolina" "south-dakota" "tennessee" "texas" "utah" "vermont" "virginia" "washington" "west-virginia" "wisconsin" "wyoming"; do
+  rm -rf "$2/$region/content/cities";
+  mv "$1/$region/content/cities" "$2/$region/content/cities";
+done
+
