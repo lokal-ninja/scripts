82032f62f6b0e94e1ed6263261fc4728bd95a72d
midzer <midzer@gmail.com>
2020-11-09 17:45:23 +0100

    add ireland

--- build/europe/all.sh
+++ build/europe/all.sh
@@ -1,5 +1,5 @@
 # Download and build all single countries in Europe
-for country in "austria" "switzerland"; do
+for country in "austria" "ireland-and-northern-ireland" "switzerland"; do
   wget "https://download.geofabrik.de/europe/$country-latest.osm.pbf"
   ./pbf2md $country
 done
