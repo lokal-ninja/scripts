7983fb89b1d54fdaf8f45744aeb0bf12bac4eadc
midzer <midzer@gmail.com>
2021-01-11 15:41:02 +0100

    improve comment

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,2 @@
-# Recursively update ln-base theme to latest version and commit for all subfolders
+# Recursively remove public and resources folder for all subfolders
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c 'cd {} && rm -rf public/ && rm -rf resources/' \;
