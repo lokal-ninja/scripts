3f24a0a827c676492e9f93e452c74164b75ae66d
midzer <midzer@gmail.com>
2021-01-11 15:42:11 +0100

    add new helper scripts

---
+++ helper/update-config.sh
@@ -0,0 +1,2 @@
+# Add new line to config.toml and commit
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c 'cd {} && sed -i "7s/$/enableGitInfo = true\n/" config.toml && git commit -am "update config.toml"' \;
