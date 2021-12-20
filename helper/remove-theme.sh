544a3126b064890842508240d48b0c0bc0ced1d6
midzer <midzer@gmail.com>
2021-12-20 17:55:16 +0100

    update remove-theme

--- helper/remove-theme.sh
+++ helper/remove-theme.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Recursively remove theme from all subfolders
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c 'cd {} && git submodule deinit -f themes/ln-base-en && rm -rf .git/modules/themes/ln-base-en && git rm -f themes/ln-base-en && git commit -m "remove theme"' \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c 'cd {} && git submodule deinit -f themes/ln-base && rm -rf .git/modules/themes/ln-base && git rm -f themes/ln-base && git commit -am "remove theme"' \;
